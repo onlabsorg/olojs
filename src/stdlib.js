9ffe79bf3b7b96703383d82aeb099662eb77ee06
Marcello Del Buono <m.delbuono@gmail.com>
2020-01-21 08:26:38 +0100

    added basic math module in stdlib

--- src/stdlib.js
+++ src/stdlib.js
@@ -1,3 +1,4 @@
 
+exports["/math"]     = () => import(/* webpackChunkName: "math" */ "../lib/stdlib/math");
 exports["/markdown"] = () => import(/* webpackChunkName: "markdown" */ "../lib/stdlib/markdown");
 exports["/html"] =     () => import(/* webpackChunkName: "html" */     "../lib/stdlib/html");
