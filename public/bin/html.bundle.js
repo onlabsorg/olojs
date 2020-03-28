51fb0f1b2fe8b854bd55147a7038a044793d9db2
Marcello Del Buono <m.delbuono@gmail.com>
2020-03-28 23:11:33 +0100

    pending changes

--- public/bin/html.bundle.js
+++ public/bin/html.bundle.js
@@ -1,1 +1,14 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{20:function(n,i,o){const a=o(2);i.__apply__=async function(n,i={},...o){var p=`<${n}`;for(let n in i){p+=` ${n}="${await a.stringify(i[n])}"`}return p+=">",p+=o.join(""),p+=`</${n}>`}}}]);
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([["/bin/html"],{
+
+/***/ "./lib/stdlib/modules/html.js":
+/*!************************************!*\
+  !*** ./lib/stdlib/modules/html.js ***!
+  \************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+eval("\nconst expression = __webpack_require__(/*! ../../expression */ \"./lib/expression.js\");\n\nasync function createElement (tagName, attributes={}, ...childElements) {\n    var html = `<${tagName}`;\n    for (let attrName in attributes) {\n        let attrValue = await expression.stringify(attributes[attrName]);\n        html += ` ${attrName}=\"${attrValue}\"`\n    }\n    html += \">\";\n    html += childElements.join(\"\");\n    html += `</${tagName}>`;\n    return html;\n}\n\nexports.__apply__ = createElement;\n\n\n//# sourceURL=webpack:///./lib/stdlib/modules/html.js?");
+
+/***/ })
+
+}]);
