f416cc10fd2731fd44a5458871d8f661fb3a99d0
Marcello Del Buono <m.delbuono@gmail.com>
2021-01-09 22:14:45 +0100

    Resolved vulnerability

--- node_modules/@onlabsorg/swan-js/lib/stdlib/path.js
+++
@@ -1,9 +0,0 @@
-const pathlib = require("path");
-
-module.exports = {
-    getBaseName: pathlib.basename,
-    getDirName: pathlib.dirname,
-    getExtName: pathlib.extname,
-    normalize: pathlib.normalize,
-    resolve: (...paths) => pathlib.resolve("/", ...paths)
-};
