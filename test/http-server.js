141501ae010c1b29b79d2d3036baace6dd64e8b2
Marcello Del Buono <m.delbuono@gmail.com>
2021-01-29 23:09:18 +0100

    Added .deleteAll support to Router and to HTTPServer

--- test/http-server.js
+++ test/http-server.js
@@ -14,32 +14,32 @@ require("isomorphic-fetch");
 
 
 describe("HTTPServer", () => {
-    
+
     describe("Default HTTPServer", () => {
         var homeStore, router, server;
 
         before((done) => {
-            
+
             class PrivateStore extends Store {
                 read (path) {throw new Store.ReadPermissionDeniedError(path)}
                 list (path) {throw new Store.ReadPermissionDeniedError(path)}
                 write (path,src) {throw new Store.WritePermissionDeniedError(path)}
                 delete (path) {throw new Store.WritePermissionDeniedError(path)}
             }
-            
+
             class NotAllowedStore extends Store {
                 read (path) {throw new Store.ReadOperationNotAllowedError(path)}
                 list (path) {throw new Store.ReadOperationNotAllowedError(path)}
                 write (path, src) {throw new Store.WriteOperationNotAllowedError(path)}
                 delete (path) {throw new Store.WriteOperationNotAllowedError(path)}
             }
-            
+
             class ErrorStore extends Store {
                 read (path) {throw new Error()}
                 write (path, src) {throw new Error()}
                 delete (path) {throw new Error()}
             }
-            
+
             homeStore = new MemoryStore();
 
             router = new Router({
@@ -48,18 +48,18 @@ describe("HTTPServer", () => {
                 '/hidden/': new NotAllowedStore(),
                 '/error/': new ErrorStore(),
             });
-                    
+
             server = HTTPServer.createServer(router);
             server.listen(8888, done);
-        });            
-        
+        });
+
         describe("HTTP GET /docs/*", () => {
-            
+
             it("should return the document source mapped to path by the passed loader", async () => {
                 var docPath = "/path/to/doc1";
                 var docSource = "document source ...";
                 await homeStore.write(docPath, docSource);
-                
+
                 var response = await fetch(`http://localhost:8888/docs/${docPath}`, {
                     method: 'get',
                     headers: {
@@ -69,7 +69,7 @@ describe("HTTPServer", () => {
                 expect(response.status).to.equal(200);
                 expect(await response.text()).to.equal(docSource);
             });
-            
+
             it("should return the status code 403 if the backend environment throws ReadPermissionDenied", async () => {
                 var response = await fetch(`http://localhost:8888/docs/private/path/to/doc`, {
                     method: 'get',
@@ -79,7 +79,7 @@ describe("HTTPServer", () => {
                 });
                 expect(response.status).to.equal(403);
             });
-            
+
             it("should return the status code 405 if the backend environment throws Store.ReadOperationNotDefined", async () => {
                 var response = await fetch(`http://localhost:8888/docs/hidden/path/to/doc`, {
                     method: 'get',
@@ -87,9 +87,9 @@ describe("HTTPServer", () => {
                         'Accept': 'text/*'
                     },
                 });
-                expect(response.status).to.equal(405);                
+                expect(response.status).to.equal(405);
             });
-            
+
             it("should return the status code 500 if the backend environment throws a generic error", async () => {
                 var response = await fetch(`http://localhost:8888/docs/error/path/to/doc`, {
                     method: 'get',
@@ -97,14 +97,14 @@ describe("HTTPServer", () => {
                         'Accept': 'text/*'
                     },
                 });
-                expect(response.status).to.equal(500);                
+                expect(response.status).to.equal(500);
             });
 
             it("should respond with the JSON-stringified entries list if the accepted MimeType is `apprication/json`", async () => {
                 var docPath = "/path/to/doc1";
                 var docSource = "document source ...";
                 await homeStore.write(docPath, docSource);
-                
+
                 var response = await fetch(`http://localhost:8888/docs/`, {
                     method: 'get',
                     headers: {
@@ -125,14 +125,14 @@ describe("HTTPServer", () => {
                 expect(response.status).to.equal(415);
             });
         });
-            
+
         describe("HTTP PUT /docs/*", () => {
-            
+
             it("should modify the resource and return 200", async () => {
                 var docPath = "/path/to/doc1";
                 var docSource = "document source ...";
                 homeStore.write(docPath, docSource);
-                
+
                 var response = await fetch(`http://localhost:8888/docs${docPath}`, {
                     method: 'put',
                     headers: {
@@ -141,11 +141,11 @@ describe("HTTPServer", () => {
                     },
                     body: "new document source ..."
                 });
-                
+
                 expect(response.status).to.equal(200);
                 expect(homeStore.read(docPath)).to.equal("new document source ...")
             });
-            
+
             it("should return the status code 403 if the backend environment throws WritePermissionDenied", async () => {
                 var response = await fetch(`http://localhost:8888/docs/private/path/to/doc`, {
                     method: 'put',
@@ -157,7 +157,7 @@ describe("HTTPServer", () => {
                 });
                 expect(response.status).to.equal(403);
             });
-            
+
             it("should return the status code 405 if the backend environment throws WriteOperationNotDefined", async () => {
                 var response = await fetch(`http://localhost:8888/docs/hidden/path/to/doc`, {
                     method: 'put',
@@ -167,9 +167,9 @@ describe("HTTPServer", () => {
                     },
                     body: ""
                 });
-                expect(response.status).to.equal(405);                
+                expect(response.status).to.equal(405);
             });
-            
+
             it("should return the status code 500 if the backend environment throws a generic error", async () => {
                 var response = await fetch(`http://localhost:8888/docs/error/path/to/doc`, {
                     method: 'put',
@@ -179,17 +179,17 @@ describe("HTTPServer", () => {
                     },
                     body: ""
                 });
-                expect(response.status).to.equal(500);                
+                expect(response.status).to.equal(500);
             });
         });
 
         describe("HTTP DELETE /docs/*", () => {
-            
+
             it("should remove the resource and return 200", async () => {
                 var docPath = "/path/to/doc1";
                 var docSource = "document source ...";
                 homeStore.write(docPath, docSource);
-                
+
                 var response = await fetch(`http://localhost:8888/docs${docPath}`, {
                     method: 'delete',
                     headers: {
@@ -197,11 +197,34 @@ describe("HTTPServer", () => {
                         'Content-Type': 'text/plain',
                     },
                 });
-                
+
                 expect(response.status).to.equal(200);
                 expect(homeStore.read(docPath)).to.equal("");
             });
-            
+
+            it("should remove all the resources matching the path if Content-Type is `text/directory`", async () => {
+                await homeStore.write("/path/to/doc1", "doc @ /path/to/doc1");
+                await homeStore.write("/path/to/dir/", "doc @ /path/to/dir/");
+                await homeStore.write("/path/to/dir/doc2", "doc @ /path/to/dir/doc2");
+
+                expect(await homeStore.read("/path/to/doc1")).to.equal("doc @ /path/to/doc1");
+                expect(await homeStore.read("/path/to/dir/")).to.equal("doc @ /path/to/dir/");
+                expect(await homeStore.read("/path/to/dir/doc2")).to.equal("doc @ /path/to/dir/doc2");
+
+                var response = await fetch(`http://localhost:8888/docs/path/to/dir`, {
+                    method: 'delete',
+                    headers: {
+                        'Accept': 'text/*',
+                        'Content-Type': 'text/directory',
+                    },
+                });
+
+                expect(response.status).to.equal(200);
+                expect(await homeStore.read("/path/to/doc1")).to.equal("doc @ /path/to/doc1");
+                expect(await homeStore.read("/path/to/dir/")).to.equal("");
+                expect(await homeStore.read("/path/to/dir/doc2")).to.equal("");
+            });
+
             it("should return the status code 403 if the backend environment throws WritePermissionDenied", async () => {
                 var response = await fetch(`http://localhost:8888/docs/private/path/to/doc`, {
                     method: 'delete',
@@ -212,7 +235,7 @@ describe("HTTPServer", () => {
                 });
                 expect(response.status).to.equal(403);
             });
-            
+
             it("should return the status code 405 if the backend environment throws WriteOperationNotDefined", async () => {
                 var response = await fetch(`http://localhost:8888/docs/hidden/path/to/doc`, {
                     method: 'delete',
@@ -221,9 +244,9 @@ describe("HTTPServer", () => {
                         'Accept': 'text/*'
                     },
                 });
-                expect(response.status).to.equal(405);                
+                expect(response.status).to.equal(405);
             });
-            
+
             it("should return the status code 500 if the backend environment throws a generic error", async () => {
                 var response = await fetch(`http://localhost:8888/docs/error/path/to/doc`, {
                     method: 'delete',
@@ -232,30 +255,30 @@ describe("HTTPServer", () => {
                         'Accept': 'text/*'
                     },
                 });
-                expect(response.status).to.equal(500);                
+                expect(response.status).to.equal(500);
             });
         });
-        
+
         describe("HTTP GET /docs/*", async () => {
-            
+
             it("should return the default index.html page if the path is '/'", async () => {
                 var indexHTMLPage = fs.readFileSync(pathlib.resolve(__dirname, "../public/index.html"), "utf8");
                 var response = await fetch(`http://localhost:8888/`);
                 expect(response.status).to.equal(200);
-                expect(await response.text()).to.equal(indexHTMLPage);           
+                expect(await response.text()).to.equal(indexHTMLPage);
             });
-            
+
             it("should return files from the public path for non `/env/*` GET requests", async () => {
                 var indexHTMLPage = fs.readFileSync(pathlib.resolve(__dirname, "../public/index.html"), "utf8");
-                var response = await fetch(`http://localhost:8888/index.html`); 
+                var response = await fetch(`http://localhost:8888/index.html`);
                 expect(response.status).to.equal(200);
-                expect(await response.text()).to.equal(indexHTMLPage);                
+                expect(await response.text()).to.equal(indexHTMLPage);
             });
         });
-        
+
         after((done) => {
             server.close(done);
-        });        
+        });
     });
 
     describe("Custom HTTPServer", () => {
@@ -263,46 +286,46 @@ describe("HTTPServer", () => {
 
         before((done) => {
             customPublicPath = pathlib.resolve(__dirname, "./public");
-            store = new MemoryStore();        
-            
+            store = new MemoryStore();
+
             const beforeMiddleware = express.Router();
-            beforeMiddleware.all(`/store/before/*`, (req, res, next) => { 
-                res.status(200).send(`@before: ${req.method} ${req.path}`); 
+            beforeMiddleware.all(`/store/before/*`, (req, res, next) => {
+                res.status(200).send(`@before: ${req.method} ${req.path}`);
             });
-            
+
             const afterMiddleware = express.Router();
-            afterMiddleware.all(`/after/*`, (req, res, next) => { 
-                res.status(200).send(`@after: ${req.method} ${req.path}`); 
+            afterMiddleware.all(`/after/*`, (req, res, next) => {
+                res.status(200).send(`@after: ${req.method} ${req.path}`);
             });
-            
+
             server = HTTPServer.createServer(store, {
                 before: beforeMiddleware,
                 storeRoute: '/store',
                 after: afterMiddleware,
                 publicPath: customPublicPath
             });
-            
+
             server.listen(8888, done);
-        });     
-        
+        });
+
         it("should call the `options.before` middleware at every request", async () => {
             var response = await fetch(`http://localhost:8888/store/before/test`);
             expect(response.status).to.equal(200);
-            expect(await response.text()).to.equal("@before: GET /store/before/test");                
-        });       
-        
+            expect(await response.text()).to.equal("@before: GET /store/before/test");
+        });
+
         it("should delegate to the store middleware if the path starts with `options.storeRoute`", async () => {
             await store.write('/path/to/doc', "Doc @ /path/to/doc");
             var response = await fetch(`http://localhost:8888/store/path/to/doc`);
             expect(response.status).to.equal(200);
-            expect(await response.text()).to.equal("Doc @ /path/to/doc");                                        
+            expect(await response.text()).to.equal("Doc @ /path/to/doc");
         });
 
         it("should call the `options.after` middleware at every request not handled by the store middleware", async () => {
             var response = await fetch(`http://localhost:8888/after/test`);
             expect(response.status).to.equal(200);
-            expect(await response.text()).to.equal("@after: GET /after/test");                            
-        });       
+            expect(await response.text()).to.equal("@after: GET /after/test");
+        });
 
         it("should return the custom index.html page if the path is '/'", async () => {
             var customIndexPage = "<p>custom html document page</p>";
@@ -310,20 +333,20 @@ describe("HTTPServer", () => {
 
             var response = await fetch(`http://localhost:8888/`);
             expect(response.status).to.equal(200);
-            expect(await response.text()).to.equal(customIndexPage);                
+            expect(await response.text()).to.equal(customIndexPage);
         });
 
         it("should return files from the public path for non-text/olo GET requests", async () => {
             var file1_content = "content of file 1";
             fs.writeFileSync(customPublicPath+"/file1.txt", file1_content, "utf8");
-            
+
             var response = await fetch(`http://localhost:8888/file1.txt`);
             expect(response.status).to.equal(200);
-            expect(await response.text()).to.equal(file1_content);                
+            expect(await response.text()).to.equal(file1_content);
         });
 
         after((done) => {
             server.close(done);
-        });           
+        });
     });
 });
--- test/router.js
+++ test/router.js
@@ -6,9 +6,9 @@ var Router = require('../lib/router');
 
 
 describe("Router", () => {
-    
+
     describe("[store, subPath] = router.match(path)", () => {
-        
+
         it("should return store mounted at the route matching the path and the subPath relative to that route", async () => {
             var routes = {
                 "path/to": new MemoryStore(),
@@ -21,8 +21,8 @@ describe("Router", () => {
             expect(await router.matchRoute('/path/to/store2/path/to/doc')).to.deep.equal([routes['/path/to/store2'], "/path/to/doc"]);
             expect(await router.matchRoute('/path/to/store2/')).to.deep.equal([routes['/path/to/store2'], "/"]);
             expect(await router.matchRoute('/path_to/doc')).to.deep.equal([routes['/'], "/path_to/doc"]);
-        });        
-        
+        });
+
         it("should return [null, path] if no match is found", () => {
             var routes = {
                 "/path/to/s1": new MemoryStore(),
@@ -32,9 +32,9 @@ describe("Router", () => {
             expect(router.matchRoute('/path/to/s3/doc')).to.deep.equal([null, "/path/to/s3/doc"]);
         });
     });
-    
+
     describe(`source = router.read(path)`, () => {
-        
+
         it("should delegate to the matching mounted store", async () => {
             var router = new Router({
                 "path/to": new MemoryStore({
@@ -47,7 +47,7 @@ describe("Router", () => {
                 }),
                 "/": new MemoryStore({
                     "/path/to/doc"  : "doc @ root:/path/to/doc",
-                    "/path_to/doc"  : "doc @ root:/path_to/doc"                    
+                    "/path_to/doc"  : "doc @ root:/path_to/doc"
                 }),
             });
             expect(await router.read('/path/to/')).to.equal("doc @ store1:/");
@@ -56,7 +56,7 @@ describe("Router", () => {
             expect(await router.read('/path/to/store2/')).to.equal("doc @ store2:/");
             expect(await router.read('/path_to/doc')).to.equal("doc @ root:/path_to/doc");
         });
-        
+
         it("should return an empty document if no match is found", async () => {
             var router = new Router({
                 "path/to": new MemoryStore({
@@ -71,9 +71,9 @@ describe("Router", () => {
             expect(await router.read('/path_to/doc')).to.equal("");
         })
     });
-    
+
     describe(`entries = router.list(id)`, () => {
-        
+
         it("should delegate to the matching mounted store", async () => {
             var memStore = new MemoryStore({
                 "/path/to/doc1": "...",
@@ -86,7 +86,7 @@ describe("Router", () => {
             var router = new Router({
                 '/stores/mem': memStore
             })
-            
+
             expect((await router.list("/stores/mem/path/to/")).sort()).to.deep.equal(['dir1/', 'dir2/', 'doc1', 'doc2']);
             expect(await router.list("/stores/mem/path/")).to.deep.equal(['to/']);
             expect(await router.list("/stores/mem/")).to.deep.equal(['path/']);
@@ -105,9 +105,9 @@ describe("Router", () => {
             var router = new Router({
                 '/stores/mem': memStore
             })
-            expect(await router.list("/path/to/dir")).to.deep.equal([]);            
+            expect(await router.list("/path/to/dir")).to.deep.equal([]);
         });
-        
+
         it("should include mounted routes in the entries list", async () => {
             var router = new Router({
                 '/store': new MemoryStore({
@@ -128,26 +128,26 @@ describe("Router", () => {
                 '/store/path/to/dir2/dir/': new MemoryStore(),
                 '/path/to/dir3': new MemoryStore(),
             })
-            expect((await router.list("/store/path/to/")).sort()).to.deep.equal(['dir1/', 'dir2/', 'doc1', 'doc2', 'doc3', 'doc4']);                        
-            expect((await router.list("/")).sort()).to.deep.equal(['doc5', 'path/', 'store/']);                        
+            expect((await router.list("/store/path/to/")).sort()).to.deep.equal(['dir1/', 'dir2/', 'doc1', 'doc2', 'doc3', 'doc4']);
+            expect((await router.list("/")).sort()).to.deep.equal(['doc5', 'path/', 'store/']);
         });
     });
-    
+
     describe(`source = router.write(id, source)`, () => {
-        
+
         it("should delegate to the matching mounted store", async () => {
             var store1 = new MemoryStore();
             var store2 = new MemoryStore();
             var router = new Router({
                 s1: store1,
                 s2: store2,
-            });                
+            });
             await router.write('/s1/path/to/doc', "doc @ store1");
             await router.write('s2/path/to/doc', "doc @ store2");
             expect(await store1.read('/path/to/doc')).to.equal("doc @ store1");
             expect(await store2.read('/path/to/doc')).to.equal("doc @ store2");
         });
-        
+
         it("should throw an error if no match is found", async () => {
             var router = new Router();
             try {
@@ -159,22 +159,22 @@ describe("Router", () => {
             }
         });
     });
-    
+
     describe(`source = router.delete(id)`, () => {
-        
+
         it("should delegate to the matching mounted store", async () => {
             var store1 = new MemoryStore();
             var store2 = new MemoryStore();
             var router = new Router({
                 s1: store1,
                 s2: store2,
-            });                
-            
+            });
+
             await store1.write('/path/to/doc', "doc @ store1");
             await router.delete('/s1/path/to/doc');
             expect(store1.read('/path/to/doc')).to.equal("");
         });
-        
+
         it("should throw an error if no match is found", async () => {
             var router = new Router();
             try {
@@ -183,7 +183,39 @@ describe("Router", () => {
             } catch (error) {
                 expect(error).to.be.instanceof(Router.WriteOperationNotAllowedError);
                 expect(error.message).to.equal('Operation not allowed: WRITE /s1/path/to/doc')
-            }                
+            }
+        })
+    });
+
+    describe(`source = router.deleteAll(id)`, () => {
+
+        it("should delegate to the matching mounted store", async () => {
+            var store1 = new MemoryStore();
+            var store2 = new MemoryStore();
+            var router = new Router({
+                s1: store1,
+                s2: store2,
+            });
+
+            var called = "";
+            store1.deleteAll = path => {
+                called = `store1.deleteAll ${path}`;
+            }
+
+            await router.deleteAll('/s1/path/to/dir');
+
+            expect(called).to.equal("store1.deleteAll /path/to/dir");
+        });
+
+        it("should throw an error if no match is found", async () => {
+            var router = new Router();
+            try {
+                await router.deleteAll('/s1/path/to/doc');
+                throw new Error("Id did not throw");
+            } catch (error) {
+                expect(error).to.be.instanceof(Router.WriteOperationNotAllowedError);
+                expect(error.message).to.equal('Operation not allowed: WRITE /s1/path/to/doc')
+            }
         })
     });
 });
