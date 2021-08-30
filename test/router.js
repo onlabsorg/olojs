375a07e9d0f0b3975884fb55c239cec35dfe9b6f
Marcello Del Buono <m.delbuono@gmail.com>
2021-08-30 20:42:26 +0200

    Added protocols and createContext to Router

--- test/router.js
+++ test/router.js
@@ -1,5 +1,6 @@
 var expect = require("chai").expect;
 var document = require("../lib/document");
+var Store = require('../lib/store');
 var MemoryStore = require('../lib/memory-store');
 var Router = require('../lib/router');
 
@@ -7,7 +8,7 @@ var Router = require('../lib/router');
 
 describe("Router", () => {
 
-    describe("iterator = router.iterRoutes()", () => {
+    describe("iterator = router._iterRoutes()", () => {
 
         it("should return an iterator yielding all the [routeId, store] pairs in analphabetic order", () => {
             var routes = {
@@ -16,8 +17,8 @@ describe("Router", () => {
                 "/": new MemoryStore()
             }
             var router = new Router(routes);
-            expect(router.iterRoutes()[Symbol.iterator]).to.be.a("function");
-            expect(Array.from(router.iterRoutes())).to.deep.equal([
+            expect(router._iterRoutes()[Symbol.iterator]).to.be.a("function");
+            expect(Array.from(router._iterRoutes())).to.deep.equal([
                 ['/path/to/store2/', routes["/path/to/store2"]],
                 ['/path/to/', routes["path/to"]],
                 ['/', routes["/"]],
@@ -34,7 +35,7 @@ describe("Router", () => {
                 "/": new MemoryStore()
             }
             var router = new Router(routes);
-            expect(Array.from(router.iterRoutes())).to.deep.equal([
+            expect(Array.from(router._iterRoutes())).to.deep.equal([
                 ['/path/to/store2/', routes["/path/to/store2"]],
                 ['/path/to/', routes["path/to"]],
                 ['/', routes["/"]],
@@ -42,7 +43,7 @@ describe("Router", () => {
 
             var store3 = new MemoryStore();
             router.mount('/path/to/new/store', store3);
-            expect(Array.from(router.iterRoutes())).to.deep.equal([
+            expect(Array.from(router._iterRoutes())).to.deep.equal([
                 ['/path/to/store2/', routes["/path/to/store2"]],
                 ['/path/to/new/store/', store3],
                 ['/path/to/', routes["path/to"]],
@@ -57,18 +58,40 @@ describe("Router", () => {
                 "/": new MemoryStore()
             }
             var router = new Router(routes);
-            expect(Array.from(router.iterRoutes())).to.deep.equal([
+            expect(Array.from(router._iterRoutes())).to.deep.equal([
                 ['/path/to/store2/', routes["/path/to/store2"]],
                 ['/path/to/', routes["path/to"]],
                 ['/', routes["/"]],
             ]);
 
             router.mount('/path/to/new/store', {});
-            expect(Array.from(router.iterRoutes())).to.deep.equal([
+            expect(Array.from(router._iterRoutes())).to.deep.equal([
                 ['/path/to/store2/', routes["/path/to/store2"]],
                 ['/path/to/', routes["path/to"]],
                 ['/', routes["/"]],
             ]);
+        });
+        
+        it("should mount uri-like routes under /.prot/<scheme>", () => {
+            const store1 = new MemoryStore();
+            const store2 = new MemoryStore();
+            const store3 = new MemoryStore();
+            
+            var router = new Router({
+                '/path/to/store1': store1,
+                '/path/to/store2': store2,
+            });
+            expect(Array.from(router._iterRoutes())).to.deep.equal([
+                ['/path/to/store2/', store2],
+                ['/path/to/store1/', store1],
+            ]);
+
+            router.mount('http:/path/to/store3', store3);
+            expect(Array.from(router._iterRoutes())).to.deep.equal([
+                ['/path/to/store2/', store2],
+                ['/path/to/store1/', store1],
+                ['/.prot/http/path/to/store3/', store3],
+            ]);            
         });
     });
 
@@ -81,21 +104,44 @@ describe("Router", () => {
                 "/": new MemoryStore()
             }
             var router = new Router(routes);
-            expect(Array.from(router.iterRoutes())).to.deep.equal([
+            expect(Array.from(router._iterRoutes())).to.deep.equal([
                 ['/path/to/store2/', routes["/path/to/store2"]],
                 ['/path/to/', routes["path/to"]],
                 ['/', routes["/"]],
             ]);
 
             router.unmount('path/to/store2');
-            expect(Array.from(router.iterRoutes())).to.deep.equal([
+            expect(Array.from(router._iterRoutes())).to.deep.equal([
                 ['/path/to/', routes["path/to"]],
                 ['/', routes["/"]],
             ]);
         });
+        
+        it("should remove uri-like routes under /.prot/<scheme>", () => {
+            const store1 = new MemoryStore();
+            const store2 = new MemoryStore();
+            const store3 = new MemoryStore();
+            
+            var router = new Router({
+                '/path/to/store1': store1,
+                '/path/to/store2': store2,
+                'http:/path/to/store3': store3
+            });
+            expect(Array.from(router._iterRoutes())).to.deep.equal([
+                ['/path/to/store2/', store2],
+                ['/path/to/store1/', store1],
+                ['/.prot/http/path/to/store3/', store3],
+            ]);
+
+            router.unmount('http:/path/to/store3', store3);
+            expect(Array.from(router._iterRoutes())).to.deep.equal([
+                ['/path/to/store2/', store2],
+                ['/path/to/store1/', store1],
+            ]);            
+        });        
     });
 
-    describe("[store, subPath] = router.match(path)", () => {
+    describe("[store, subPath] = router._match(path)", () => {
 
         it("should return store mounted at the route matching the path and the subPath relative to that route", () => {
             var routes = {
@@ -104,11 +150,11 @@ describe("Router", () => {
                 "/": new MemoryStore()
             }
             var router = new Router(routes);
-            expect(router.match('/path/to/')).to.deep.equal([routes['path/to'], "/"]);
-            expect(router.match('/path/to/doc')).to.deep.equal([routes['path/to'], "/doc"]);
-            expect(router.match('/path/to/store2/path/to/doc')).to.deep.equal([routes['/path/to/store2'], "/path/to/doc"]);
-            expect(router.match('/path/to/store2/')).to.deep.equal([routes['/path/to/store2'], "/"]);
-            expect(router.match('/path_to/doc')).to.deep.equal([routes['/'], "/path_to/doc"]);
+            expect(router._match('/path/to/')).to.deep.equal([routes['path/to'], "/"]);
+            expect(router._match('/path/to/doc')).to.deep.equal([routes['path/to'], "/doc"]);
+            expect(router._match('/path/to/store2/path/to/doc')).to.deep.equal([routes['/path/to/store2'], "/path/to/doc"]);
+            expect(router._match('/path/to/store2/')).to.deep.equal([routes['/path/to/store2'], "/"]);
+            expect(router._match('/path_to/doc')).to.deep.equal([routes['/'], "/path_to/doc"]);
         });
 
         it("should return [null, path] if no match is found", () => {
@@ -117,7 +163,7 @@ describe("Router", () => {
                 "/path/to/s2": new MemoryStore(),
             }
             var router = new Router(routes);
-            expect(router.match('/path/to/s3/doc')).to.deep.equal([null, "/path/to/s3/doc"]);
+            expect(router._match('/path/to/s3/doc')).to.deep.equal([null, "/path/to/s3/doc"]);
         });
     });
 
@@ -158,6 +204,17 @@ describe("Router", () => {
             });
             expect(await router.read('/path_to/doc')).to.equal("");
         })
+        
+        it("should resolve uri-like paths", async () => {
+            const router = new Router({
+                "http:/path/to/store1": new MemoryStore({
+                    "/doc1"  : "doc1 @ http:/path/to/store1",
+                    "/doc2"  : "doc2 @ http:/path/to/store1"
+                }),
+            });
+            expect(await router.read('http:/path/to/store1/doc1')).to.equal("doc1 @ http:/path/to/store1");
+            expect(await router.read('/.prot/http/path/to/store1/doc2')).to.equal("doc2 @ http:/path/to/store1");
+        });
     });
 
     describe(`entries = router.list(id)`, () => {
@@ -219,6 +276,22 @@ describe("Router", () => {
             expect((await router.list("/store/path/to/")).sort()).to.deep.equal(['dir1/', 'dir2/', 'doc1', 'doc2', 'doc3', 'doc4']);
             expect((await router.list("/")).sort()).to.deep.equal(['doc5', 'path/', 'store/']);
         });
+        
+        it("should resolve uri-like paths", async () => {
+            const router = new Router({
+                "http:/path1/to/doc": new MemoryStore(),
+                "http:/path2/to/doc": new MemoryStore(),
+                "ftp:/path/to/dir1/": new MemoryStore(),
+                "ftp:/path/to/dir1/doc1": new MemoryStore(),
+                "/path/to/dir1/doc2": new MemoryStore(),
+                "/path/to/dir2/": new MemoryStore(),
+            });
+
+            expect(await router.list("/")).to.deep.equal(['path/', '.prot/']);
+            expect(await router.list("http:/")).to.deep.equal(['path2/', 'path1/']);
+            expect(await router.list("http:/path1")).to.deep.equal(['to/']);
+            expect(await router.list("/.prot/")).to.deep.equal(['http/', 'ftp/']);
+        });        
     });
 
     describe(`source = router.write(id, source)`, () => {
@@ -245,6 +318,21 @@ describe("Router", () => {
                 expect(error).to.be.instanceof(Router.WriteOperationNotAllowedError);
                 expect(error.message).to.equal('Operation not allowed: WRITE /s1/path/to/doc')
             }
+        });
+        
+        it("should resolve uri-like paths", async () => {
+            var store1 = new MemoryStore();
+            var store2 = new MemoryStore();
+            var router = new Router({
+                'http:/store1': store1,
+                'http:/store2': store2,
+            });
+            
+            await router.write('http:/store1/path/to/doc', "doc @ store1");
+            expect(await store1.read('/path/to/doc')).to.equal("doc @ store1");
+            
+            await router.write('/.prot/http/store2/path/to/doc', "doc @ store2");
+            expect(await store2.read('/path/to/doc')).to.equal("doc @ store2");
         });
     });
 
@@ -273,6 +361,27 @@ describe("Router", () => {
                 expect(error.message).to.equal('Operation not allowed: WRITE /s1/path/to/doc')
             }
         })
+        
+        it("should resolve uri-like paths", async () => {
+            var store1 = new MemoryStore({
+                '/path/to/doc': "doc @ store1"
+            });
+            var store2 = new MemoryStore({
+                '/path/to/doc': "doc @ store2"                
+            });
+            var router = new Router({
+                'http:/store1': store1,
+                'http:/store2': store2,
+            });
+            
+            expect(await store1.read('/path/to/doc')).to.equal("doc @ store1");
+            await router.delete('http:/store1/path/to/doc');
+            expect(await store1.read('/path/to/doc')).to.equal("");
+            
+            expect(await store2.read('/path/to/doc')).to.equal("doc @ store2");
+            await router.delete('/.prot/http/store2/path/to/doc');
+            expect(await store2.read('/path/to/doc')).to.equal("");
+        });        
     });
 
     describe(`source = router.deleteAll(id)`, () => {
@@ -305,5 +414,103 @@ describe("Router", () => {
                 expect(error.message).to.equal('Operation not allowed: WRITE /s1/path/to/doc')
             }
         })
+
+        it("should resolve uri-like paths", async () => {
+            var store1 = new MemoryStore();
+            var store2 = new MemoryStore();
+            var router = new Router({
+                'http:/store1': store1,
+                'http:/store2': store2,
+            });
+
+            var called = "";
+            store1.deleteAll = path => {
+                called = `store1.deleteAll ${path}`;
+            }
+
+            await router.deleteAll('http:/store1/path/to/dir');
+
+            expect(called).to.equal("store1.deleteAll /path/to/dir");
+        });
+    });
+    
+    describe(`context = router.createContext(path, presets)`, () => {
+        
+        it("should delegate the context creation to the first matching store", () => {
+            const store1 = new MemoryStore();
+            const store2 = Object.create(store1);
+            store2.createContext = (path, presets) => {
+                const context = store1.createContext(path, presets);
+                context.storeName = "store2";
+                return context;
+            }
+            var router = new Router({
+                '/store1': store1,
+                '/store2': store2
+            });
+            
+            const ctx = router.createContext('/store2/path/to/doc', {x:10});
+            expect(ctx.x).to.equal(10);
+            expect(ctx.storeName).to.equal("store2");
+            expect(ctx.__path__).to.equal('/store2/path/to/doc');
+        });
+
+        it("should delegate to the parent store if no match is found", () => {
+            Store.contextPrototype.storeName = "void store";
+            
+            var router = new Router({
+                '/store1': new MemoryStore(),
+                '/store2': new MemoryStore()
+            });
+            
+            const ctx = router.createContext('/store3/path/to/doc', {x:10});
+            expect(ctx.x).to.equal(10);
+            expect(ctx.storeName).to.equal("void store");
+            expect(ctx.__path__).to.equal('/store3/path/to/doc');
+
+            delete Store.contextPrototype.storeName;
+        });
+        
+        it("should resolve uri-like paths", () => {
+            const store1 = new MemoryStore();
+            const store2 = Object.create(store1);
+            store2.createContext = (path, presets) => {
+                const context = store1.createContext(path, presets);
+                context.storeName = "store2";
+                return context;
+            }
+            var router = new Router({
+                'http:/store1': store1,
+                'http:/store2': store2
+            });
+            
+            const ctx = router.createContext('http:/store2/path/to/doc', {x:10});
+            expect(ctx.x).to.equal(10);
+            expect(ctx.storeName).to.equal("store2");
+            expect(ctx.__path__).to.equal('/.prot/http/store2/path/to/doc');
+        });
+        
+        describe("context.import function", () => {
+            
+            it("should resolve paths relative to the router, not to the context store", async () => {
+                const router = new Router({
+                    '/store1': new MemoryStore({
+                        '/path/to/doc1': "<% docnum = 1 %>",
+                        '/path/to/doc2': "<% docnum = 2 %>",
+                        '/path/to/doc3': "<% docnum = 3 %>",
+                    })
+                });
+                
+                const ctx1 = await router.createContext('/store1/path/to/doc1');
+                
+                const doc2_ns = await ctx1.import('doc2');
+                expect(doc2_ns.docnum).to.equal(2);
+                expect(doc2_ns.__path__).to.equal('/store1/path/to/doc2')
+
+                const doc3_ns = await ctx1.import('/store1/path/to/doc3');
+                expect(doc3_ns.docnum).to.equal(3);                
+                expect(doc3_ns.__path__).to.equal('/store1/path/to/doc3')
+            });
+        });
     });
 });
