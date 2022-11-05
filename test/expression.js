4b0c658a4fd8f98f169924736b8019e41c39636e
Marcello Del Buono <m.delbuono@gmail.com>
2022-11-05 14:13:21 +0100

    Removed swan 'markdown' module

--- test/expression.js
+++ test/expression.js
@@ -2,19 +2,3 @@ var expect = require("chai").expect;
 var expression = require('../lib/expression');
 
 require('@onlabsorg/swan-js/test/index');
-
-describe("olojs expression stdlib", () => {
-
-    describe("markdown module", () => {
-
-        it("should expose a function that converts markdown to HTML", async () => {
-            var evaluate = expression.parse(`{
-                markdown = require 'markdown',
-                html = markdown '*bold*'
-            }.html`);
-            console.log(await evaluate(expression.createContext()))
-            var html = await evaluate(expression.createContext());
-            expect(html).to.equal("<p><em>bold</em></p>\n");
-        });
-    });
-});
