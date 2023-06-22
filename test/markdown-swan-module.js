795bac9182dc083b955a91e08c6a83b343772d68
Marcello Del Buono <m.delbuono@gmail.com>
2023-06-22 22:31:42 +0200

    markdown module: fixed test bug

--- test/markdown-swan-module.js
+++ test/markdown-swan-module.js
@@ -5,9 +5,10 @@ const olojs = require("..");
 describe("markdown swan module", () => {
 
     it("should expose a function that converts markdown to HTML", async () => {
-        const store = new olojs.MemoryStore();
-        const doc = store.createDocument('/path/to/doc',
-                "<% require 'markdown' '*bold*' %>");
+        const store = new olojs.MemoryStore({
+            '/test/doc': "<% require 'markdown' '*bold*' %>"
+        });
+        const doc = await store.loadDocument('/test/doc');
         const docns = await doc.evaluate(doc.createContext());
         expect(docns.__text__).to.equal("<p><em>bold</em></p>\n");
     });
