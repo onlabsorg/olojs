fa18db39a3acfca0a8a39264ea9c234b3c62c55f
Marcello Del Buono <m.delbuono@gmail.com>
2023-06-30 23:15:08 +0200

    Built v0.28.1

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ olo.MemoryStore = require('./lib/memory-store');
 olo.FileStore = require('./lib/file-store');
 olo.HTTPStore = require('./lib/http-store');
 olo.Router = require('./lib/router');
+olo.URIStore = require('./lib/uri-store');
 
 olo.HTTPServer = require('./lib/http-server');
 
